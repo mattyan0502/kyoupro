6598c702081532de91a48924d1b52bfe14b36668
mattyan0502 <74781822+mattyan0502@users.noreply.github.com>
2020-11-22 00:32:33 +0900

    firstCommit

---
+++ KyougiProgramming/KyougiProgramming/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KyougiProgramming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Atcoder.ABC.ABC014B.Main(args);
+        }
+    }
+}
