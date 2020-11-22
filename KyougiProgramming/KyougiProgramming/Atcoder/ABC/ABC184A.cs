29f2646a45283b50e69596b79de5f1effe3abf8f
mattyan0502 <mattyanprogramming@gmail.com>
2020-11-22 21:41:57 +0900

    test

---
+++ KyougiProgramming/KyougiProgramming/Atcoder/ABC/ABC184A.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KyougiProgramming.Atcoder.ABC
+{
+    class ABC184A
+    {
+        public static void Main(string[]args)
+        {
+            string[] str1 = Console.ReadLine().Split(' ');
+            string[] str2 = Console.ReadLine().Split(' ');
+            int a = int.Parse(str1[0]);
+            int b = int.Parse(str1[1]);
+            int c = int.Parse(str2[0]);
+            int d = int.Parse(str2[1]);
+
+            int ans = a * d - b * c;
+            Console.WriteLine(ans);
+
+        }
+    }
+}
---
+++ KyougiProgramming/KyougiProgramming/Atcoder/ABC/ABC184B.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KyougiProgramming.Atcoder.ABC
+{
+    class ABC184B
+    {
+        public static void Main(string[] args)
+        {
+            string[] a = Console.ReadLine().Split(' ');
+            int n = int.Parse(a[0]);
+            int X = int.Parse(a[1]);
+            string S = Console.ReadLine();
+            long ans = X;
+            for(int i = 0; i < S.Length; i++)
+            {
+                if(S[i] == 'o')
+                {
+                    ans = ans + 1;
+                }
+                else if(S[i] == 'x' && ans > 0)
+                {
+                    ans = ans - 1;
+                }
+            }
+            Console.WriteLine(ans);
+        }
+    }
+}
